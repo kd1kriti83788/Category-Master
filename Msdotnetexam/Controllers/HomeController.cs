4c16b262bd490c8664dd5f4de910c9febbef3a90
kriti <kritikeshri1@gmail.com>
2024-12-11 15:50:36 +0530

    project

---
+++ Msdotnetexam/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Msdotnetexam.Models;
+using System.Diagnostics;
+
+
+namespace Msdotnetexam.Controllers
+{
+    public class HomeController : Controller
+    {
+        TrainingDbContext context = null;
+
+        public HomeController(TrainingDbContext context)
+        {
+            this.context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View("Login");
+        }
+
+        
+        public IActionResult AfterLogin(Institute institute)
+        {
+            String message = "Invalid credentials";
+            foreach (var ins in context.Institute)
+            {
+                if (ins.Code.Equals(institute.Code) && institute.Pin.Equals(ins.Pin))
+                {
+                    return View("Create");
+                }
+            }
+
+            return View("Login", message);
+        }
+
+
+        [HttpGet]
+                public IActionResult Edit()
+                {
+                   return View("Edit");
+                }
+
+                [HttpPost]
+                public IActionResult Edit(Courses course, int? id)
+                {
+
+
+            Courses productDelete = context.Courses.Find(id);
+
+                        context.Courses.Update(course);
+
+                        context.SaveChanges();
+           
+
+                   return Redirect("/home/Course");
+                }
+
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Login");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Msdotnetexam/Models/Institute.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Msdotnetexam.Models
+{
+    [Table("Institute")]
+    public class Institute
+    {
+        [Column("Code", TypeName = "varchar(10)")]
+        [Key]
+        public string Code { get; set; }
+
+        [Column("Name", TypeName = "varchar(100)")]
+        public string Name { get; set; }
+
+
+        [Column("Pin", TypeName = "varchar(10)")]
+        public string Pin { get; set; }
+    }
+
+    [Table("Courses")]
+    public class Courses
+    {
+        [Column("Id", TypeName = "int")]
+        [Key]
+        public int Id { get; set; }
+
+        [Column("Course", TypeName = "varchar(100)")]
+        public string Course { get; set; }
+
+
+        [Column("Fees", TypeName = "int")]
+        public int StockQuantity { get; set; }
+
+
+
+        [Column("Code", TypeName = "varchar(10)")]
+        public string Code { get; set; }
+    }
+
+    public class TrainingDbContext : DbContext
+    {
+        public TrainingDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Institute> Institute { get; set; }
+
+        public DbSet<Courses> Courses { get; set; }
+
+    }
+}
